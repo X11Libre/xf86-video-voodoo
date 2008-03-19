b5b0845ed8ef7a2fa2e771961c1155f32258392e
Adam Jackson <ajax@redhat.com>
2008-03-19 17:07:12 -0400

    Death to RCS tags.

--- src/voodoo_driver.c
+++ src/voodoo_driver.c
@@ -39,7 +39,6 @@
  * ANY KIND OR FORM.
  */
 
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/voodoo/voodoo_driver.c,v 1.27 2001/08/07 07:04:46 keithp Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
