3065954a83c966901baecf686619e6679833632f
Alan Coopersmith <alan.coopersmith@oracle.com>
2024-01-16 16:38:58 -0800

    voodoo_find_dac doesn't check device_ids (#2)
    Closes: #2
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- src/voodoo_hardware.c
+++ src/voodoo_hardware.c
@@ -389,10 +389,10 @@ static int voodoo_find_dac(VoodooPtr pVoo)
 	device_id = dac_in(pVoo, 2);
 	
 	/* AT&T 20C409 and clones */
-	if(vendor_id == DAC_VENDOR_ATT && DAC_DEVICE_ATT20C409)
+	if(vendor_id == DAC_VENDOR_ATT && device_id == DAC_DEVICE_ATT20C409)
 		return DAC_ID_ATT;
 		
-	if(vendor_id == DAC_VENDOR_TI && DAC_DEVICE_TITVP3409)
+	if(vendor_id == DAC_VENDOR_TI && device_id == DAC_DEVICE_TITVP3409)
 		return DAC_ID_TI;
 		
 	/* ICS5432 doesn't implement the back door. Glide does some
